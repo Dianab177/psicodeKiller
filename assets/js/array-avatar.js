4bf0cca32578b2f80e6608a3855d77122f13095b
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2022-11-28 08:31:40 +0100

    array image

---
+++ assets/js/array-avatar.js
@@ -0,0 +1,32 @@
+let coders = [
+  {
+    nombre: "adria",
+    castigo: "Despedida",
+    src: "./assets/images/avatar/adria.png",
+  },
+  {
+    nombre: "alba",
+    castigo: "Despadida",
+    src: "./assets/images/avatar/alba.png",
+  },
+  {
+    nombre: "adria",
+    castigo: "Despadida",
+  },
+  {
+    nombre: "adria",
+    castigo: "Despadida",
+  },
+  {
+    nombre: "adria",
+    castigo: "Despadida",
+  },
+  {
+    nombre: "adria",
+    castigo: "Despadida",
+  },
+  {
+    nombre: "adria",
+    castigo: "Despadida",
+  },
+];
